66ab3dba49d63ebd94485aa05df642fd318f6006
sreenima <sreenima.p95@gmail.com>
2020-05-17 15:11:05 +0530

    Initial Commit

---
+++ Chicking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chicking
+//
+//  Created by sreenima95 on 17/05/20.
+//  Copyright © 2020 sreenima95. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
